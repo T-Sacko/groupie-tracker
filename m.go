d287018c73b8cd545d1225373f20c333b21d3a22
titi <sackotiti@gmail.com>
2022-12-23 17:25:04 +0000

    gt

--- m.go
+++ m.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -19,16 +18,44 @@ type T []struct {
 	relations    string `json: "relations"`
 }
 
-var Take T
+type U struct {
+	Id           int    `json: "id"`
+	Image        string `json: "image"`
+	Name         string `json: "name"`
+	Members      string `json: "members"`
+	CreationDate int    `json: "creationDate"`
+	FirstAlbum   string `json: "firstAlbum"`
+	Locations    string `json: "locations"`
+	ConcertDates string `json: "concertDates"`
+	Relations    string `json: "relations"`
+}
+
+var (
+	Take T
+	Art  []U
+)
 
 func Data() {
 	response, _ := http.Get("http://groupietrackers.herokuapp.com/api/artists")
 	defer response.Body.Close()
 	file, _ := ioutil.ReadAll(response.Body)
-	h, _ := json.Unmarshal(file, T)
-	return h
+	h := json.Unmarshal(file, &Take)
+	// make sure to manage the error and return and Http.Status(Internalservererror)
+	for _, v := range Take {
+		Art = append(Art, U{
+			Id:           v.ID,
+			Image:        v.Image,
+			Name:         v.Name,
+			Members:      v.Members,
+			CreationDate: v.CreationDate,
+			FirstAlbum:   v.FirstAlbum,
+			Locations:    v.Locations,
+			ConcertDates: v.ConcertDates,
+			Relations:    v.Relations
+		})
+	}
 }
 
 func main() {
-	fmt.Println(Data())
+	// fmt.Println(Data())
 }

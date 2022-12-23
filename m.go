1ce0e9119a0eaa1cdd3173af0ea54d69a0d3751d
titi <sackotiti@gmail.com>
2022-12-23 18:32:08 +0000

    gt

--- m.go
+++ m.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 type T []struct {
-	id           int    `json: "id"`
-	image        string `json: "image"`
-	name         string `json: "name"`
-	members      string `json: "members"`
-	creationDate int    `json: "creationDate"`
-	firstAlbum   string `json: "firstAlbum"`
-	locations    string `json: "locations"`
-	concertDates string `json: "concertDates"`
-	relations    string `json: "relations"`
+	Id           int    `json: "id"`
+	Image        string `json: "image"`
+	Name         string `json: "name"`
+	Members      string `json: "members"`
+	CreationDate int    `json: "creationDate"`
+	FirstAlbum   string `json: "firstAlbum"`
+	Locations    string `json: "locations"`
+	ConcertDates string `json: "concertDates"`
+	Relations    string `json: "relations"`
 }
 
 type U struct {
@@ -39,11 +40,12 @@ func Data() {
 	response, _ := http.Get("http://groupietrackers.herokuapp.com/api/artists")
 	defer response.Body.Close()
 	file, _ := ioutil.ReadAll(response.Body)
-	h := json.Unmarshal(file, &Take)
+	_ = json.Unmarshal(file, &Take)
+
 	// make sure to manage the error and return and Http.Status(Internalservererror)
 	for _, v := range Take {
 		Art = append(Art, U{
-			Id:           v.ID,
+			Id:           v.Id,
 			Image:        v.Image,
 			Name:         v.Name,
 			Members:      v.Members,
@@ -51,11 +53,12 @@ func Data() {
 			FirstAlbum:   v.FirstAlbum,
 			Locations:    v.Locations,
 			ConcertDates: v.ConcertDates,
-			Relations:    v.Relations
+			Relations:    v.Relations,
 		})
 	}
+	fmt.Println(Art)
 }
 
 func main() {
-	// fmt.Println(Data())
+	Data()
 }

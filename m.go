6cb519dbc728003c9cc0d3d6f7020d1337706344
titi <sackotiti@gmail.com>
2022-12-23 15:03:32 +0000

    gt

---
+++ m.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	type t struct {
+		id           int
+		image        string
+		name         string
+		members      string
+		creationDate int
+		firstAlbum   string
+		locations    string
+		concertDates string
+		relations    string
+	}
+}

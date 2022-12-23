d07d0ba98c2b1dd43865e1744a4428bcca6952df
titi <sackotiti@gmail.com>
2022-12-23 16:35:15 +0000

    gt

--- m.go
+++ m.go
@@ -1,15 +1,34 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type T []struct {
+	id           int    `json: "id"`
+	image        string `json: "image"`
+	name         string `json: "name"`
+	members      string `json: "members"`
+	creationDate int    `json: "creationDate"`
+	firstAlbum   string `json: "firstAlbum"`
+	locations    string `json: "locations"`
+	concertDates string `json: "concertDates"`
+	relations    string `json: "relations"`
+}
+
+var Take T
+
+func Data() {
+	response, _ := http.Get("http://groupietrackers.herokuapp.com/api/artists")
+	defer response.Body.Close()
+	file, _ := ioutil.ReadAll(response.Body)
+	h, _ := json.Unmarshal(file, T)
+	return h
+}
+
 func main() {
-	type t struct {
-		id           int
-		image        string
-		name         string
-		members      string
-		creationDate int
-		firstAlbum   string
-		locations    string
-		concertDates string
-		relations    string
-	}
+	fmt.Println(Data())
 }

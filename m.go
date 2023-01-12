de0ab6689234c05bac44e72c554671609936de4b
titi <sackotiti@gmail.com>
2023-01-12 23:08:39 +0000

    groupie-tracker

--- m.go
+++ m.go
@@ -3,48 +3,65 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
 
+var (
+	Take T
+	Data []U
+)
+
 type T []struct {
-	Id           int    `json: "id"`
-	Image        string `json: "image"`
-	Name         string `json: "name"`
-	Members      string `json: "members"`
-	CreationDate int    `json: "creationDate"`
-	FirstAlbum   string `json: "firstAlbum"`
-	Locations    string `json: "locations"`
-	ConcertDates string `json: "concertDates"`
-	Relations    string `json: "relations"`
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
 type U struct {
-	Id           int    `json: "id"`
-	Image        string `json: "image"`
-	Name         string `json: "name"`
-	Members      string `json: "members"`
-	CreationDate int    `json: "creationDate"`
-	FirstAlbum   string `json: "firstAlbum"`
-	Locations    string `json: "locations"`
-	ConcertDates string `json: "concertDates"`
-	Relations    string `json: "relations"`
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
-var (
-	Take T
-	Art  []U
-)
+func groupie(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("index.html")
+	tmpl.Execute(w, struct{ Take T }{Take})
 
-func Data() {
-	response, _ := http.Get("http://groupietrackers.herokuapp.com/api/artists")
+}
+
+func main() {
+	response, err := http.Get("http://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		print(err)
+	}
 	defer response.Body.Close()
 	file, _ := ioutil.ReadAll(response.Body)
 	_ = json.Unmarshal(file, &Take)
+	fmt.Println(GetData()[0].Image)
+	http.HandleFunc("/", groupie)
+	http.ListenAndServe(":5505", nil)
+
+}
+
+func GetData() []U {
 
 	// make sure to manage the error and return and Http.Status(Internalservererror)
 	for _, v := range Take {
-		Art = append(Art, U{
+		Data = append(Data, U{
 			Id:           v.Id,
 			Image:        v.Image,
 			Name:         v.Name,
@@ -56,9 +73,5 @@ func Data() {
 			Relations:    v.Relations,
 		})
 	}
-	fmt.Println(Art)
-}
-
-func main() {
-	Data()
+	return (Data)
 }

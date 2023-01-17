f82b73f21b0d3444ff64e617a400023a89badda8
titi <sackotiti@gmail.com>
2023-01-17 18:52:36 +0000

    groupie

--- m.go
+++ m.go
@@ -25,25 +25,23 @@ var (
 	Locations loc
 )
 
-type loc []struct {
-	Id        int    `json:"id"`
-	Dates     int    `json:"dates"`
-	Locations string `json:"locations"`
+type loc struct {
+	Id        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     string   `json:"dates"`
 }
 
 func main() {
 	response, err := http.Get("http://groupietrackers.herokuapp.com/api/artists")
-
 	if err != nil {
 		print(err)
 	}
 	defer response.Body.Close()
 	file, _ := ioutil.ReadAll(response.Body)
 	_ = json.Unmarshal(file, &Take)
-	fmt.Println(Take[0].Image)
+	fmt.Println(Take[2].Name)
 	http.HandleFunc("/", groupie)
 	http.ListenAndServe(":5505", nil)
-
 }
 
 func groupie(w http.ResponseWriter, r *http.Request) {
@@ -53,26 +51,24 @@ func groupie(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, struct{ Take T }{Take})
 
 	} else {
-		fmt.Println("gogo")
-		tmp, _ := template.ParseFiles("id.html")
-		tmp.Execute(w, nil)
-		// locs, err := http.Get("https://groupietrackers.herokuapp.com/api/locations")
-		// if err != nil {
-		// 	print(err)
-		// }
-		// defer locs.Body.Close()
-		// file, _ := ioutil.ReadAll(locs.Body)
-		// _ = json.Unmarshal(file, &Locations)
-		// dates, errr := http.Get("https://groupietrackers.herokuapp.com/api/dates")
-		// if errr != nil {
-		// 	print(err)
-		// }
-		// defer locs.Body.Close()
-		// filedates, _ := ioutil.ReadAll(dates.Body)
-		// _ = json.Unmarshal(filedates, &Locations)
-		// fmt.Println(Locations)
+		fmt.Fprint(w, "<html><body>404 error</body></html>")
 	}
+	// fmt.Println("gogo")
+	// nn := len(r.URL.Path) - 1
+	// n := r.URL.Path[nn:]
+	// sn, _ := strconv.Atoi(n)
+	// locs, err := http.Get(Take[sn-1].Locations)
+	// if err != nil {
+	// 	print(err)
+	// }
+	// defer locs.Body.Close()
+	// file, _ := ioutil.ReadAll(locs.Body)
+	// _ = json.Unmarshal(file, &Locations)
+	// tmp, _ := template.ParseFiles("id.html")
+	// tmp.Execute(w, Locations)
+	// fmt.Println(Locations.Id)
 
+	// }
 }
 
 // func GetData() []U {
